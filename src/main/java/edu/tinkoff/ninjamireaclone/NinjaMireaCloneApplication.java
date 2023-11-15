6bcbe06aa0701fb69a3b373f179f3083ebe901ca
sejapoe <bekaldiev2015@gmail.com>
2023-11-15 16:08:13 +0300

    chore(git): initial commit

---
+++ src/main/java/edu/tinkoff/ninjamireaclone/NinjaMireaCloneApplication.java
@@ -0,0 +1,13 @@
+package edu.tinkoff.ninjamireaclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaMireaCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinjaMireaCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tinkoff/ninjamireaclone/NinjaMireaCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tinkoff.ninjamireaclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaMireaCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
