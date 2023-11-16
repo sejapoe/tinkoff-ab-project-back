1a7cb4a030f06fea124764167f4539a9bc8b8d3e
sejapoe <bekaldiev2015@gmail.com>
2023-11-16 19:08:00 +0300

    fix: fix test system (now use 'test' profile for all tests)

--- src/test/java/edu/tinkoff/ninjamireaclone/NinjaMireaCloneApplicationTests.java
+++ src/test/java/edu/tinkoff/ninjamireaclone/NinjaMireaCloneApplicationTests.java
@@ -2,12 +2,13 @@ package edu.tinkoff.ninjamireaclone;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NinjaMireaCloneApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
