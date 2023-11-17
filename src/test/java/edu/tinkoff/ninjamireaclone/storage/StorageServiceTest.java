1eec651deae2a0d8632e73643ebb94bd3995c870
sejapoe <bekaldiev2015@gmail.com>
2023-11-17 18:37:49 +0300

    test: enhance StorageServiceTest with more test scenarios and environments

--- src/test/java/edu/tinkoff/ninjamireaclone/storage/StorageServiceTest.java
+++ src/test/java/edu/tinkoff/ninjamireaclone/storage/StorageServiceTest.java
@@ -1,30 +1,48 @@
 package edu.tinkoff.ninjamireaclone.storage;
 
 import edu.tinkoff.ninjamireaclone.config.StorageProperties;
+import edu.tinkoff.ninjamireaclone.exception.storage.StorageException;
+import edu.tinkoff.ninjamireaclone.exception.storage.StorageFileNotFoundException;
+import edu.tinkoff.ninjamireaclone.model.Document;
+import edu.tinkoff.ninjamireaclone.repository.DocumentRepository;
 import edu.tinkoff.ninjamireaclone.service.storage.StorageService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class StorageServiceTest {
     @Autowired
     private StorageService storageService;
 
     @Autowired
     private StorageProperties storageProperties;
+    @Autowired
+    private DocumentRepository documentRepository;
+
+    @BeforeEach
+    public void clear() {
+        storageService.deleteAll();
+        storageService.init();
+    }
 
     @Test
-    public void mockFileStoreTest() {
+    public void storeFileTest() throws IOException {
         // given
         MultipartFile file = new MockMultipartFile(
                 "test.txt",
@@ -39,5 +57,91 @@ public class StorageServiceTest {
         // then
         Path location = Paths.get(storageProperties.getLocation());
         assertThat(Files.exists(location)).isTrue();
+        assertThat(Files.list(location)).anySatisfy(path ->
+                assertThat(path.getFileName().toString()).endsWith("test.txt"));
+    }
+
+    @Test
+    public void storeFileOutsideDirectoryTest() throws IOException {
+        // given
+        MultipartFile file = new MockMultipartFile(
+                "../test.txt",
+                "../test.txt",
+                "text/plain",
+                "Lorem ipsum dolores".getBytes(Charset.defaultCharset())
+        );
+
+        // when
+        Exception e = assertThrows(Exception.class, () -> storageService.store(file));
+
+        // then
+        Path location = Paths.get(storageProperties.getLocation());
+        assertThat(e).isInstanceOf(StorageException.class);
+        assertThat(e.getMessage()).isEqualTo("Unable to store file outside current directory");
+        assertThat(Files.exists(location)).isTrue();
+        assertThat(Files.list(location)).noneSatisfy(path ->
+                assertThat(path.getFileName().toString()).endsWith("test.txt"));
+    }
+
+    @Test
+    public void storeEmptyFileTest() throws IOException {
+        // given
+        MultipartFile file = new MockMultipartFile(
+                "test.txt",
+                "test.txt",
+                "text/plain",
+                "".getBytes(Charset.defaultCharset())
+        );
+
+        // when
+        Exception e = assertThrows(Exception.class, () -> storageService.store(file));
+
+        // then
+        Path location = Paths.get(storageProperties.getLocation());
+        assertThat(e).isInstanceOf(StorageException.class);
+        assertThat(e.getMessage()).isEqualTo("Failed to store empty file");
+        assertThat(Files.exists(location)).isTrue();
+        assertThat(Files.list(location)).noneSatisfy(path ->
+                assertThat(path.getFileName().toString()).endsWith("test.txt"));
+    }
+
+    @Test
+    public void loadFileAsResourceTest() {
+        // given
+        MultipartFile file = new MockMultipartFile(
+                "test.txt",
+                "test.txt",
+                "text/plain",
+                "Lorem ipsum dolores".getBytes(Charset.defaultCharset())
+        );
+        storageService.store(file);
+        Document document = documentRepository.findAll().get(0);
+
+        // when
+        Resource resource = storageService.loadAsResource(document.getFilename());
+
+        // then
+        assertThat(resource.exists()).isTrue();
+        assertThat(resource.isReadable()).isTrue();
+        assertThat(resource.getFilename()).endsWith("test.txt");
+    }
+
+    @Test
+    public void loadFileAsResourceNotFoundTest() {
+        // given
+        MultipartFile file = new MockMultipartFile(
+                "test.txt",
+                "test.txt",
+                "text/plain",
+                "Lorem ipsum dolores".getBytes(Charset.defaultCharset())
+        );
+        storageService.store(file);
+
+        // when
+        Exception e = assertThrows(Exception.class, () -> storageService.loadAsResource("not-found.txt"));
+
+        // then
+        assertThat(e).isInstanceOf(StorageFileNotFoundException.class);
+        assertThat(e.getMessage()).isEqualTo("Failed to read file: not-found.txt");
     }
 }
