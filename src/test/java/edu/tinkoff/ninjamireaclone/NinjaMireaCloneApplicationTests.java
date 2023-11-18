ec8b34b245b9656898d5918487219e47e6483f8b
JudexMars <danielpro2013rus@gmail.com>
2023-11-18 11:21:12 +0300

    fix: add active profiles for 2 tests

--- src/test/java/edu/tinkoff/ninjamireaclone/NinjaMireaCloneApplicationTests.java
+++ src/test/java/edu/tinkoff/ninjamireaclone/NinjaMireaCloneApplicationTests.java
@@ -2,8 +2,10 @@ package edu.tinkoff.ninjamireaclone;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NinjaMireaCloneApplicationTests {
 
     @Test
