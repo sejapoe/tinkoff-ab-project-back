c46ce20f90e3913196a068a611250e3f0adeac32
sejapoe <bekaldiev2015@gmail.com>
2023-11-23 16:55:50 +0300

    feat: add pagination in topic posts

--- src/main/java/edu/tinkoff/ninjamireaclone/dto/topic/response/TopicResponseDto.java
+++ src/main/java/edu/tinkoff/ninjamireaclone/dto/topic/response/TopicResponseDto.java
@@ -1,15 +1,14 @@
 package edu.tinkoff.ninjamireaclone.dto.topic.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import edu.tinkoff.ninjamireaclone.dto.common.PageResponseDto;
 import edu.tinkoff.ninjamireaclone.dto.post.response.PostResponseDto;
-
-import java.util.List;
 
 public record TopicResponseDto(
         long id,
         String name,
         @JsonProperty("parent_id")
         Long parentId,
-        List<PostResponseDto> posts
+        PageResponseDto<PostResponseDto> posts
 ) {
 }
