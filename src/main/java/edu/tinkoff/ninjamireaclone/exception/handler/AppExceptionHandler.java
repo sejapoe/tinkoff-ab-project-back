15cd2e889102e7486af263ea65f2ea589cdd795b
sejapoe <bekaldiev2015@gmail.com>
2023-11-24 23:31:46 +0300

    feat: add file size limit

--- src/main/java/edu/tinkoff/ninjamireaclone/exception/handler/AppExceptionHandler.java
+++ src/main/java/edu/tinkoff/ninjamireaclone/exception/handler/AppExceptionHandler.java
@@ -2,14 +2,21 @@ package edu.tinkoff.ninjamireaclone.exception.handler;
 
 import edu.tinkoff.ninjamireaclone.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @RestControllerAdvice
 public class AppExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<String> handleClientError(NotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ProblemDetail handleMaxUploadSizeExceed(MaxUploadSizeExceededException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.PAYLOAD_TOO_LARGE, ex.getMessage());
     }
 }
